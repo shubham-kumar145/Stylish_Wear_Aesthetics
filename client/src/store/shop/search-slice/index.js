e2441a704743b9fab266192cb0446b3ab2de087a
Shubham Kumar <shubhamkr5923@gmail.com>
2026-04-03 22:32:22 +0530

    Update index.js

--- client/src/store/shop/search-slice/index.js
+++ client/src/store/shop/search-slice/index.js
@@ -1,3 +1,51 @@
+// import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+// import axios from "axios";
+
+// const initialState = {
+//   isLoading: false,
+//   searchResults: [],
+// };
+
+// export const getSearchResults = createAsyncThunk(
+//   "/order/getSearchResults",
+//   async (keyword) => {
+//     const response = await axios.get(
+//       `http://localhost:5000/api/shop/search/${keyword}`
+//     );
+
+//     return response.data;
+//   }
+// );
+
+// const searchSlice = createSlice({
+//   name: "searchSlice",
+//   initialState,
+//   reducers: {
+//     resetSearchResults: (state) => {
+//       state.searchResults = [];
+//     },
+//   },
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(getSearchResults.pending, (state) => {
+//         state.isLoading = true;
+//       })
+//       .addCase(getSearchResults.fulfilled, (state, action) => {
+//         state.isLoading = false;
+//         state.searchResults = action.payload.data;
+//       })
+//       .addCase(getSearchResults.rejected, (state) => {
+//         state.isLoading = false;
+//         state.searchResults = [];
+//       });
+//   },
+// });
+
+// export const { resetSearchResults } = searchSlice.actions;
+
+// export default searchSlice.reducer;
+
+
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
@@ -6,13 +54,14 @@ const initialState = {
   searchResults: [],
 };
 
+const baseURL = import.meta.env.VITE_API_URL;
+
 export const getSearchResults = createAsyncThunk(
   "/order/getSearchResults",
   async (keyword) => {
     const response = await axios.get(
-      `http://localhost:5000/api/shop/search/${keyword}`
+      `${baseURL}/api/shop/search/${keyword}`
     );
-
     return response.data;
   }
 );
