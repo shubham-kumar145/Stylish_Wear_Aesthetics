2a58dc243143839014f250e88a0e01461d090fd3
Shubham Kumar <shubhamkr5923@gmail.com>
2026-04-03 22:30:38 +0530

    Update index.js

--- client/src/store/shop/products-slice/index.js
+++ client/src/store/shop/products-slice/index.js
@@ -1,3 +1,84 @@
+// import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+// import axios from "axios";
+
+// const initialState = {
+//   isLoading: false,
+//   productList: [],
+//   productDetails: null,
+// };
+
+// export const fetchAllFilteredProducts = createAsyncThunk(
+//   "/products/fetchAllProducts",
+//   async ({ filterParams, sortParams }) => {
+//     console.log(fetchAllFilteredProducts, "fetchAllFilteredProducts");
+
+//     const query = new URLSearchParams({
+//       ...filterParams,
+//       sortBy: sortParams,
+//     });
+
+//     const result = await axios.get(
+//       `http://localhost:5000/api/shop/products/get?${query}`
+//     );
+
+//     console.log(result);
+
+//     return result?.data;
+//   }
+// );
+
+// export const fetchProductDetails = createAsyncThunk(
+//   "/products/fetchProductDetails",
+//   async (id) => {
+//     const result = await axios.get(
+//       `http://localhost:5000/api/shop/products/get/${id}`
+//     );
+
+//     return result?.data;
+//   }
+// );
+
+// const shoppingProductSlice = createSlice({
+//   name: "shoppingProducts",
+//   initialState,
+//   reducers: {
+//     setProductDetails: (state) => {
+//       state.productDetails = null;
+//     },
+//   },
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(fetchAllFilteredProducts.pending, (state, action) => {
+//         state.isLoading = true;
+//       })
+//       .addCase(fetchAllFilteredProducts.fulfilled, (state, action) => {
+//         state.isLoading = false;
+//         state.productList = action.payload.data;
+//       })
+//       .addCase(fetchAllFilteredProducts.rejected, (state, action) => {
+//         state.isLoading = false;
+//         state.productList = [];
+//       })
+//       .addCase(fetchProductDetails.pending, (state, action) => {
+//         state.isLoading = true;
+//       })
+//       .addCase(fetchProductDetails.fulfilled, (state, action) => {
+//         state.isLoading = false;
+//         state.productDetails = action.payload.data;
+//       })
+//       .addCase(fetchProductDetails.rejected, (state, action) => {
+//         state.isLoading = false;
+//         state.productDetails = null;
+//       });
+//   },
+// });
+
+// export const { setProductDetails } = shoppingProductSlice.actions;
+
+// export default shoppingProductSlice.reducer;
+
+
+
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
@@ -7,21 +88,19 @@ const initialState = {
   productDetails: null,
 };
 
+const baseURL = import.meta.env.VITE_API_URL;
+
 export const fetchAllFilteredProducts = createAsyncThunk(
   "/products/fetchAllProducts",
   async ({ filterParams, sortParams }) => {
-    console.log(fetchAllFilteredProducts, "fetchAllFilteredProducts");
-
     const query = new URLSearchParams({
       ...filterParams,
       sortBy: sortParams,
     });
 
     const result = await axios.get(
-      `http://localhost:5000/api/shop/products/get?${query}`
+      `${baseURL}/api/shop/products/get?${query}`
     );
-
-    console.log(result);
 
     return result?.data;
   }
@@ -31,7 +110,7 @@ export const fetchProductDetails = createAsyncThunk(
   "/products/fetchProductDetails",
   async (id) => {
     const result = await axios.get(
-      `http://localhost:5000/api/shop/products/get/${id}`
+      `${baseURL}/api/shop/products/get/${id}`
     );
 
     return result?.data;
@@ -48,25 +127,25 @@ const shoppingProductSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
-      .addCase(fetchAllFilteredProducts.pending, (state, action) => {
+      .addCase(fetchAllFilteredProducts.pending, (state) => {
         state.isLoading = true;
       })
       .addCase(fetchAllFilteredProducts.fulfilled, (state, action) => {
         state.isLoading = false;
         state.productList = action.payload.data;
       })
-      .addCase(fetchAllFilteredProducts.rejected, (state, action) => {
+      .addCase(fetchAllFilteredProducts.rejected, (state) => {
         state.isLoading = false;
         state.productList = [];
       })
-      .addCase(fetchProductDetails.pending, (state, action) => {
+      .addCase(fetchProductDetails.pending, (state) => {
         state.isLoading = true;
       })
       .addCase(fetchProductDetails.fulfilled, (state, action) => {
         state.isLoading = false;
         state.productDetails = action.payload.data;
       })
-      .addCase(fetchProductDetails.rejected, (state, action) => {
+      .addCase(fetchProductDetails.rejected, (state) => {
         state.isLoading = false;
         state.productDetails = null;
       });
