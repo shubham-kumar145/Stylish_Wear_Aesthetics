d1f6f5a6bd832bf267ea8c71015381607c61b844
Shubham Kumar <shubhamkr5923@gmail.com>
2026-04-03 22:26:25 +0530

    Update index.js

--- client/src/store/shop/address-slice/index.js
+++ client/src/store/shop/address-slice/index.js
@@ -1,3 +1,87 @@
+// import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// import axios from "axios";
+
+// const initialState = {
+//   isLoading: false,
+//   addressList: [],
+// };
+
+// export const addNewAddress = createAsyncThunk(
+//   "/addresses/addNewAddress",
+//   async (formData) => {
+//     const response = await axios.post(
+//       "http://localhost:5000/api/shop/address/add",
+//       formData
+//     );
+
+//     return response.data;
+//   }
+// );
+
+// export const fetchAllAddresses = createAsyncThunk(
+//   "/addresses/fetchAllAddresses",
+//   async (userId) => {
+//     const response = await axios.get(
+//       `http://localhost:5000/api/shop/address/get/${userId}`
+//     );
+
+//     return response.data;
+//   }
+// );
+
+// export const editaAddress = createAsyncThunk(
+//   "/addresses/editaAddress",
+//   async ({ userId, addressId, formData }) => {
+//     const response = await axios.put(
+//       `http://localhost:5000/api/shop/address/update/${userId}/${addressId}`,
+//       formData
+//     );
+
+//     return response.data;
+//   }
+// );
+
+// export const deleteAddress = createAsyncThunk(
+//   "/addresses/deleteAddress",
+//   async ({ userId, addressId }) => {
+//     const response = await axios.delete(
+//       `http://localhost:5000/api/shop/address/delete/${userId}/${addressId}`
+//     );
+
+//     return response.data;
+//   }
+// );
+
+// const addressSlice = createSlice({
+//   name: "address",
+//   initialState,
+//   reducers: {},
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(addNewAddress.pending, (state) => {
+//         state.isLoading = true;
+//       })
+//       .addCase(addNewAddress.fulfilled, (state, action) => {
+//         state.isLoading = false;
+//       })
+//       .addCase(addNewAddress.rejected, (state) => {
+//         state.isLoading = false;
+//       })
+//       .addCase(fetchAllAddresses.pending, (state) => {
+//         state.isLoading = true;
+//       })
+//       .addCase(fetchAllAddresses.fulfilled, (state, action) => {
+//         state.isLoading = false;
+//         state.addressList = action.payload.data;
+//       })
+//       .addCase(fetchAllAddresses.rejected, (state) => {
+//         state.isLoading = false;
+//         state.addressList = [];
+//       });
+//   },
+// });
+
+// export default addressSlice.reducer;
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
@@ -6,14 +90,15 @@ const initialState = {
   addressList: [],
 };
 
+const baseURL = import.meta.env.VITE_API_URL;
+
 export const addNewAddress = createAsyncThunk(
   "/addresses/addNewAddress",
   async (formData) => {
     const response = await axios.post(
-      "http://localhost:5000/api/shop/address/add",
+      `${baseURL}/api/shop/address/add`,
       formData
     );
-
     return response.data;
   }
 );
@@ -22,9 +107,8 @@ export const fetchAllAddresses = createAsyncThunk(
   "/addresses/fetchAllAddresses",
   async (userId) => {
     const response = await axios.get(
-      `http://localhost:5000/api/shop/address/get/${userId}`
+      `${baseURL}/api/shop/address/get/${userId}`
     );
-
     return response.data;
   }
 );
@@ -33,10 +117,9 @@ export const editaAddress = createAsyncThunk(
   "/addresses/editaAddress",
   async ({ userId, addressId, formData }) => {
     const response = await axios.put(
-      `http://localhost:5000/api/shop/address/update/${userId}/${addressId}`,
+      `${baseURL}/api/shop/address/update/${userId}/${addressId}`,
       formData
     );
-
     return response.data;
   }
 );
@@ -45,9 +128,8 @@ export const deleteAddress = createAsyncThunk(
   "/addresses/deleteAddress",
   async ({ userId, addressId }) => {
     const response = await axios.delete(
-      `http://localhost:5000/api/shop/address/delete/${userId}/${addressId}`
+      `${baseURL}/api/shop/address/delete/${userId}/${addressId}`
     );
-
     return response.data;
   }
 );
