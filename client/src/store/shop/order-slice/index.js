5ce00d1be3ce2d136f1e00976c253309a4c20615
Shubham Kumar <shubhamkr5923@gmail.com>
2026-04-03 22:29:44 +0530

    Update index.js

--- client/src/store/shop/order-slice/index.js
+++ client/src/store/shop/order-slice/index.js
@@ -1,3 +1,121 @@
+// import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+// import axios from "axios";
+
+// const initialState = {
+//   approvalURL: null,
+//   isLoading: false,
+//   orderId: null,
+//   orderList: [],
+//   orderDetails: null,
+// };
+
+// export const createNewOrder = createAsyncThunk(
+//   "/order/createNewOrder",
+//   async (orderData) => {
+//     const response = await axios.post(
+//       "http://localhost:5000/api/shop/order/create",
+//       orderData
+//     );
+
+//     return response.data;
+//   }
+// );
+
+// export const capturePayment = createAsyncThunk(
+//   "/order/capturePayment",
+//   async ({ paymentId, payerId, orderId }) => {
+//     const response = await axios.post(
+//       "http://localhost:5000/api/shop/order/capture",
+//       {
+//         paymentId,
+//         payerId,
+//         orderId,
+//       }
+//     );
+
+//     return response.data;
+//   }
+// );
+
+// export const getAllOrdersByUserId = createAsyncThunk(
+//   "/order/getAllOrdersByUserId",
+//   async (userId) => {
+//     const response = await axios.get(
+//       `http://localhost:5000/api/shop/order/list/${userId}`
+//     );
+
+//     return response.data;
+//   }
+// );
+
+// export const getOrderDetails = createAsyncThunk(
+//   "/order/getOrderDetails",
+//   async (id) => {
+//     const response = await axios.get(
+//       `http://localhost:5000/api/shop/order/details/${id}`
+//     );
+
+//     return response.data;
+//   }
+// );
+
+// const shoppingOrderSlice = createSlice({
+//   name: "shoppingOrderSlice",
+//   initialState,
+//   reducers: {
+//     resetOrderDetails: (state) => {
+//       state.orderDetails = null;
+//     },
+//   },
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(createNewOrder.pending, (state) => {
+//         state.isLoading = true;
+//       })
+//       .addCase(createNewOrder.fulfilled, (state, action) => {
+//         state.isLoading = false;
+//         state.approvalURL = action.payload.approvalURL;
+//         state.orderId = action.payload.orderId;
+//         sessionStorage.setItem(
+//           "currentOrderId",
+//           JSON.stringify(action.payload.orderId)
+//         );
+//       })
+//       .addCase(createNewOrder.rejected, (state) => {
+//         state.isLoading = false;
+//         state.approvalURL = null;
+//         state.orderId = null;
+//       })
+//       .addCase(getAllOrdersByUserId.pending, (state) => {
+//         state.isLoading = true;
+//       })
+//       .addCase(getAllOrdersByUserId.fulfilled, (state, action) => {
+//         state.isLoading = false;
+//         state.orderList = action.payload.data;
+//       })
+//       .addCase(getAllOrdersByUserId.rejected, (state) => {
+//         state.isLoading = false;
+//         state.orderList = [];
+//       })
+//       .addCase(getOrderDetails.pending, (state) => {
+//         state.isLoading = true;
+//       })
+//       .addCase(getOrderDetails.fulfilled, (state, action) => {
+//         state.isLoading = false;
+//         state.orderDetails = action.payload.data;
+//       })
+//       .addCase(getOrderDetails.rejected, (state) => {
+//         state.isLoading = false;
+//         state.orderDetails = null;
+//       });
+//   },
+// });
+
+// export const { resetOrderDetails } = shoppingOrderSlice.actions;
+
+// export default shoppingOrderSlice.reducer;
+
+
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
@@ -9,14 +127,15 @@ const initialState = {
   orderDetails: null,
 };
 
+const baseURL = import.meta.env.VITE_API_URL;
+
 export const createNewOrder = createAsyncThunk(
   "/order/createNewOrder",
   async (orderData) => {
     const response = await axios.post(
-      "http://localhost:5000/api/shop/order/create",
+      `${baseURL}/api/shop/order/create`,
       orderData
     );
-
     return response.data;
   }
 );
@@ -25,14 +144,9 @@ export const capturePayment = createAsyncThunk(
   "/order/capturePayment",
   async ({ paymentId, payerId, orderId }) => {
     const response = await axios.post(
-      "http://localhost:5000/api/shop/order/capture",
-      {
-        paymentId,
-        payerId,
-        orderId,
-      }
+      `${baseURL}/api/shop/order/capture`,
+      { paymentId, payerId, orderId }
     );
-
     return response.data;
   }
 );
@@ -41,9 +155,8 @@ export const getAllOrdersByUserId = createAsyncThunk(
   "/order/getAllOrdersByUserId",
   async (userId) => {
     const response = await axios.get(
-      `http://localhost:5000/api/shop/order/list/${userId}`
+      `${baseURL}/api/shop/order/list/${userId}`
     );
-
     return response.data;
   }
 );
@@ -52,9 +165,8 @@ export const getOrderDetails = createAsyncThunk(
   "/order/getOrderDetails",
   async (id) => {
     const response = await axios.get(
-      `http://localhost:5000/api/shop/order/details/${id}`
+      `${baseURL}/api/shop/order/details/${id}`
     );
-
     return response.data;
   }
 );
