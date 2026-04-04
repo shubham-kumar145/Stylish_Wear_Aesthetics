04198f43dd5edb6536cfea399c858c8f5825fe29
Shubham Kumar <shubhamkr5923@gmail.com>
2026-04-04 19:26:26 +0530

    Refactor PayPal configuration code

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -5,10 +5,4 @@ paypal.configure({
   client_id: process.env.PAYPAL_CLIENT_ID,
   client_secret: process.env.PAYPAL_CLIENT_SECRET,
 });
-// paypal.configure({
-//   mode: "sandbox",
-//   client_id: "Aaiq_77dOvmvg7j53TwuAOlxRTbwaijTFnHSEOKF20L0DlJBzbO1vlFo1z_zk0QsAn8S1H0IYi6KhHqB",
-//   client_secret: "ENb33IKu1XK5eIEkdE1sTWLRzgTZD8jhObbaE9WcAc0tBw-JVjv9MRieWZAdM-3bWtrMq31r4hVBU7Cz",
-// });
-
 module.exports = paypal;
